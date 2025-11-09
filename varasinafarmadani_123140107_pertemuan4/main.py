7f6de770583c5dd7f542403025015459dd65f353
sinavarasina <varasinafarmadani@gmail.com>
2025-11-09 08:57:47 +0700

    adding list and comment

--- varasinafarmadani_123140107_pertemuan4/main.py
+++ varasinafarmadani_123140107_pertemuan4/main.py
@@ -10,7 +10,38 @@ from nilai import (
 
 
 def main():
-    mahasiswa_list = []
+    # Sorry bang saya salah paham sama tugas nya tadi, pada bagian
+    # Buat list berisi minimal 5 dictionary data mahasiswa
+    # Setiap mahasiswa memiliki: nama, NIM, nilai_uts, nilai_uas, nilai_tugas
+    # Saya kira 5 dictionary itu maksudnya nama, NIM, nilai_uts, nilai_uas, dan
+    # nilai_tugas,
+    # Saya barusadar ketika teman memberitahu kalau min 5 yang di maksud itu
+    # adalah minimal 5 data sudah di buat.
+
+    mahasiswa_list = [
+        {"nama": "Rintarou Okabe", "NIM": "23SG14001",
+            "nilai_uts": 78, "nilai_uas": 82, "nilai_tugas": 70},
+        {"nama": "Mayuri Shiina", "NIM": "23SG14002",
+            "nilai_uts": 60, "nilai_uas": 65, "nilai_tugas": 70},
+        {"nama": "Itaru Hashida", "NIM": "23SG14003",
+            "nilai_uts": 85, "nilai_uas": 80, "nilai_tugas": 88},
+        {"nama": "Kurisu Makise", "NIM": "23SG14004",
+            "nilai_uts": 95, "nilai_uas": 98, "nilai_tugas": 93},
+        {"nama": "Moeka Kiryu", "NIM": "23SG14005",
+            "nilai_uts": 83, "nilai_uas": 77, "nilai_tugas": 79},
+        {"nama": "Luka Urushibara", "NIM": "23SG14006",
+            "nilai_uts": 72, "nilai_uas": 75, "nilai_tugas": 80},
+        {"nama": "Faris NyanNyan", "NIM": "23SG14007",
+            "nilai_uts": 70, "nilai_uas": 68, "nilai_tugas": 85},
+        {"nama": "Suzuha Amane", "NIM": "23SG14008",
+            "nilai_uts": 88, "nilai_uas": 84, "nilai_tugas": 90},
+        {"nama": "Maho Hiyajo", "NIM": "23SG14009",
+            "nilai_uts": 91, "nilai_uas": 94, "nilai_tugas": 89},
+        {"nama": "Kagari Shiina", "NIM": "23SG14010",
+            "nilai_uts": 82, "nilai_uas": 85, "nilai_tugas": 80},
+        {"nama": "Yuki Amane", "NIM": "23SG14011",
+            "nilai_uts": 77, "nilai_uas": 81, "nilai_tugas": 76},
+    ]
 
     while True:
         print("\n=== Menu ===")
